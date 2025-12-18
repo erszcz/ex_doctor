bd1fce9cae0395a1b8d4b3f06154c9a0e95dcec5
Radek Szymczyszyn <lavrin@gmail.com>
2025-12-18 18:33:30 +0100

    Define to_map/1 helper for :tr and :node records

--- lib/ex_doctor.ex
+++ lib/ex_doctor.ex
@@ -31,4 +31,17 @@ defmodule ExDoctor do
   for {name, fields} <- @records do
     Record.defrecord(name, fields)
   end
+
+  def fields(record) when record in [:tr, :node] do
+    Keyword.keys(@records[record])
+  end
+
+  def to_map(record) when Record.is_record(record, :tr) or Record.is_record(record, :node) do
+    record
+    |> elem(0)
+    |> fields()
+    |> Enum.with_index(1)
+    |> Map.new(fn {field, index} -> {field, elem(record, index)} end)
+    |> Map.put(:record, elem(record, 0))
+  end
 end
--- test/ex_doctor_test.exs
+++ test/ex_doctor_test.exs
@@ -1,8 +1,35 @@
 defmodule ExDoctorTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest ExDoctor
 
-  test "greets the world" do
-    assert ExDoctor.hello() == :world
+  require ExDoctor
+  import ExDoctor, only: [to_map: 1, tr: 1]
+
+  describe "to_map/1" do
+    test "converts tr()" do
+      pid = :erlang.list_to_pid(~c"<0.115.0>")
+
+      trace =
+        tr(
+          index: 5,
+          pid: pid,
+          event: :call,
+          mfa: {:lists, :keyfind, 3},
+          data: [:syntax_colors, 1, []],
+          ts: 1_766_078_114_348_924,
+          info: :no_info
+        )
+
+      assert %{
+               record: :tr,
+               index: 5,
+               pid: ^pid,
+               event: :call,
+               mfa: {:lists, :keyfind, 3},
+               data: [:syntax_colors, 1, []],
+               ts: 1_766_078_114_348_924,
+               info: :no_info
+             } = to_map(trace)
+    end
   end
 end
